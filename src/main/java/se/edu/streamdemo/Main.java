ac41ebf131b50977b6317cec9d9b0084525fdc47
AngBryan <AngBryan@protonmail.com>
2026-09-18 17:12:33 +0800

    print using streams

--- src/main/java/se/edu/streamdemo/Main.java
+++ src/main/java/se/edu/streamdemo/Main.java
@@ -47,4 +47,12 @@ public class Main {
         }
     }
 
+    public static void printDeadlinesUsingStreams(ArrayList<Task> tasks){
+        tasks.parallelStream()
+                .filter(t -> t instanceof Deadline)
+                .forEach(System.out::println);
+
+
+    }
+
 }
